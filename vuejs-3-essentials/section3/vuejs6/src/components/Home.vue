482932f44d508a7a4eb7beaea37f5646eaf98008
nonna <ukr.nonna@gmail.com>
2021-11-07 18:04:35 -0500

    created a new vuejs project

--- vuejs-3-essentials/section3/vuejs6/src/components/Home.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <div>
-        <h1>The Home Page</h1>
-        <hr>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore aut impedit sapiente eos sit error, porro qui suscipit mollitia ad.</p>
-    </div>
-</template>
