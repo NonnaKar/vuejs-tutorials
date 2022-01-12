6a743dc413ba503127133f36296a748a7660cf79
nonna <ukr.nonna@gmail.com>
2022-01-11 22:06:54 -0500

    created components and store

--- vuex-tuts/src/main.js
+++ vuex-tuts/src/main.js
@@ -1,4 +1,28 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import { createStore } from "vuex";
 
-createApp(App).mount('#app')
+import App from "./App.vue";
+
+// Create a new store instance.
+const store = createStore({
+  state() {
+    return {
+      counter: 0
+    };
+  },
+  mutations: {
+    increment(state) {
+      state.counter = state.counter + 2;
+    },
+    increase(state, payload) {
+      state.counter = state.counter + payload.value;
+    }
+  }
+});
+
+const app = createApp(App);
+
+// Install the store instance as a plugin
+app.use(store);
+
+app.mount("#app");
